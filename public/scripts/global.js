ab94015be9665074a540b1dc11371749508d6e66
Duncan McIsaac <dbmcisaac1@gmail.com>
2016-03-27 13:35:26 -0400

    ended up moving logging code to own file because that was more convenient (I forgot that multiple global objects were instantiated)

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -9,51 +9,6 @@ function Global() {
 	// msg.pitch = 2; //0 to 2
 	msg.lang = 'en-US';
 	this.msg = msg;
-
-	// Logging
-	this.LOG = {
-		participant_id: undefined,
-		fingers: undefined,
-		participant_userAgent: navigator.userAgent, // I'll parse for OS later
-		participant_browser: determineBrowser(),
-		task_id: undefined,
-		task_start_time: Date.now(),
-		task_end_time: undefined,
-		interactions: []
-	}
-}
-
-function determineBrowser() {
-	// http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
-		// Opera 8.0+
-	var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-	    // Firefox 1.0+
-	var isFirefox = typeof InstallTrigger !== 'undefined';
-	    // At least Safari 3+: "[object HTMLElementConstructor]"
-	var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-	    // Internet Explorer 6-11
-	var isIE = /*@cc_on!@*/false || !!document.documentMode;
-	    // Edge 20+
-	var isEdge = !isIE && !!window.StyleMedia;
-	    // Chrome 1+
-	var isChrome = !!window.chrome && !!window.chrome.webstore;
-	if (isChrome)  return "Chrome";
-	if (isEdge)    return "Edge";
-	if (isIE)      return "Internet Explorer";
-	if (isSafari)  return "Safari";
-	if (isFirefox) return "Firefox";
-	if (isOpera)   return "Opera";
-}
-
-Global.prototype.addInteraction = function(key, userEvent, text) {
-	var interaction = {
-		time: Date.now(),
-		key: key,
-		userEvent: userEvent,
-		text: text
-	}
-	this.LOG.interactions.push(interaction);
-	console.log(this.LOG);
 }
 
 Global.prototype.cell = function(attributes) {
@@ -82,7 +37,7 @@ Global.prototype.cell = function(attributes) {
 			$(this).css('background-color', 'blue');
 			// add interaction when search is not focused
 			if (!$('#search input:focus').length) {
-				that.addInteraction(event.keyCode, 'hover', that.msg.text);
+				log.addInteraction(event.keyCode, 'hover', that.msg.text);
 			}
 			// cancel speech
 			speechSynthesis.cancel();
